30dd03de23240dee4b4497d37e047039466fd3e1
simplicioanderson <anderson25eu@gmail.com>
2021-05-13 02:39:25 -0300

    configurando url

--- app/core/views.py
+++ app/core/views.py
@@ -2,10 +2,30 @@
 from django.shortcuts import render
 
 def index(request):
-    template='index.html'
+    template='core/index.html'
     context = {
-        'title':'little amazon'
+        'title':'Amazon'
     }
     return render(request,template,context=context)
 
+def contato(request):
+    template='core/contact.html'
+    context = {
+        'title':'Contato'
+    }
+    
+    return render(request,template,context=context)
 
+def produto(request):
+    template='core/product.html'
+    context = {
+        'title':'Produto'
+    }
+    return render(request,template,context=context)
+
+def lista_produto(request):
+    template='core/product_list.html'
+    context = {
+        'title':'Lista de Produtos'
+    }
+    return render(request,template,context=context)
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from app.core.views import index
+from app.core import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',index)
+    path('',views.index,name='index'),
+    path('contato',views.contato,name='contato'),
+    path('produto',views.produto,name='produto'),
+    path('produtos',views.lista_produto,name='lista_produtos'),
 ]

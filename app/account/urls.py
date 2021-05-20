ad8124005b220c03662cc8092109aba952b04191
simplicioanderson <anderson25eu@gmail.com>
2021-05-20 13:33:02 -0300

    Tela de Conta de Usuário

--- app/account/urls.py
+++ app/account/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView,IndexView,UpdateUserView,PasswordView
 
 urlpatterns = [
+    path('',IndexView.as_view(),name='index'),
     path('registro/',RegisterView.as_view(),name='registro'),
+    path('alterar-dados/',UpdateUserView.as_view(),name='alterarDados'),
+    path('alterar-senha/',PasswordView.as_view(),name='update_password'),
 ]
--- app/account/views.py
+++ app/account/views.py
@@ -1,12 +1,37 @@
+from django.http import request
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView,TemplateView,UpdateView,FormView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import User
 from .forms import UserAdminCreationForm
+from django.contrib.auth.forms import PasswordChangeForm
 
+class IndexView(LoginRequiredMixin,TemplateView):
+    template_name='account/index.html'
 
 class RegisterView(CreateView):
     form_class = UserAdminCreationForm
     template_name = 'account/registro.html'
     model = User
     success_url =reverse_lazy('index')
+
+class UpdateUserView(LoginRequiredMixin,UpdateView):
+    model = User
+    template_name = "account/update_user.html"
+    fields = ['name','email']
+    success_url =reverse_lazy('account:index')
+    
+    def get_object(self):
+        return self.request.user
+
+
+class PasswordView(LoginRequiredMixin,FormView):
+    template_name = "account/update_password.html"
+    form_class = PasswordChangeForm
+    success_url =reverse_lazy('account:index')
+    
+    def get_form_kwargs(self):
+        kwargs = super(PasswordView,self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
